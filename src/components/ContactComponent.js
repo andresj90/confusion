24f10d46027b239df48d75c467d0a50059f19ded
andresj90 <andresf.jaran@konradlorenz.edu.co>
2020-07-20 20:59:10 -0500

    React redux form validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -229,6 +229,11 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 //        )     
 //     }
 // }
+const required = (val) => val && val.length; 
+const maxLength = (len) => (val) => !(val) || (val.length <=len);
+const minLength = (len) => (val) => val && (val.length >= len); 
+const isNumber = (val) => !isNaN(Number(val)); 
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Contact extends Component {
 
@@ -245,11 +250,6 @@ class Contact extends Component {
         }
 
         render(){
-            const required = (val) => val && val.length; 
-            const maxLength = (len) => (val) => !(val) || (val.length <=len);
-            const minLength = (len) => (val) => val && (val.length >= len); 
-            const isNumber = (val) => !isNaN(Number(val)); 
-            const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
             return (
                 <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
